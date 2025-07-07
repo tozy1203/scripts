f7960043bb696223966ffaf18a30644d890ed0fc
tozy <41546143@qq.com>
2025-07-07 19:46:18 +0800

    fix aria2 脚本保留

--- scripts/install_aria2.sh
+++ scripts/install_aria2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 下载并执行 aria2.sh 脚本
-bash -c "$(curl -sL https://github.com/P3TERX/aria2.sh/raw/refs/heads/master/aria2.sh)"
+curl -sL https://github.com/P3TERX/aria2.sh/raw/refs/heads/master/aria2.sh -o aria2.sh && bash aria2.sh
