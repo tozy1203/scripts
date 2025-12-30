fe7a4df575d75d60b0860fed6d7e76c60a56db3d
tozy1203 <tozy1203@users.noreply.github.com>
2025-12-30 10:31:54 +0800

    Update OpenList installation script to use v4

--- scripts/install_openlist.sh
+++ scripts/install_openlist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -fsSL "https://docs.openlist.team/v3.sh" -o v3.sh && bash v3.sh install /root
+#curl -fsSL "https://docs.openlist.team/v3.sh" -o v3.sh && bash v3.sh install /root
+curl -fsSL https://res.oplist.org/script/v4.sh > install-openlist-v4.sh && bash install-openlist-v4.sh
